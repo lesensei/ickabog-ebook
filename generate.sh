152a6c598e25ae0eb3c3ad15d06224f990ab79e1
Alain Perry <alain.perry@gmail.com>
2020-06-04 22:52:30 +0200

    First try at internationalization

--- generate.sh
+++ generate.sh
@@ -2,69 +2,62 @@
 
 mkdir -p html
 
+LC="/fr" # empty for english
 HTML_FILE=ickabog.html
-echo "<html><head><title>The Ickabog</title></head><body>" > "$HTML_FILE"
+MAIN_TITLE='The Ickabog'
+LANG="en-US"
+echo "<html><head><title>$MAIN_TITLE</title></head><body>" > "$HTML_FILE"
 
 function download_chapter() {
-    [ -s "html/$2.html" ] || wget --quiet "https://www.theickabog.com/$1" -O "html/$2.html"
+	[[ $2 =~ 1$ ]] && MAIN_TITLE=$3
+    [ -s "html/$2.html" ] || wget --quiet "https://www.theickabog.com$1" -O "html/$2.html"
+    echo "<h2>$3</h2>" >> "$HTML_FILE"
+    cat "html/$2.html" | pup 'article div.row:nth-child(2) div.entry-content' >> "$HTML_FILE"
 }
 
-download_chapter "king-fred-the-fearless/" "ch1"
-download_chapter "the-ickabog/" "ch2"
-download_chapter "death-of-a-seamstress/" "ch3"
-download_chapter "the-quiet-house/" "ch4"
-download_chapter "daisy-dovetail/" "ch5"
-download_chapter "the-fight-in-the-courtyard/" "ch6"
-download_chapter "lord-spittleworth-tells-tales/" "ch7"
-download_chapter "the-day-of-petition/" "ch8"
-download_chapter "the-shepherds-story/" "ch9"
-download_chapter "king-freds-quest/" "ch10"
-download_chapter "the-journey-north/" "ch11"
-download_chapter "the-kings-lost-sword/" "ch12"
-download_chapter "the-accident/" "ch13"
-download_chapter "lord-spittleworths-plan/" "ch14"
-download_chapter "the-king-returns/" "ch15"
-
-for i in $(seq 1 15); do
-  CHAPTER_TITLE=$(cat "html/ch$i.html" | pup 'h1.entry-title:nth-child(2) text{}')
-  echo "<h2>$CHAPTER_TITLE</h2>" >> "$HTML_FILE"
-  cat "html/ch$i.html" | pup 'article div.row:nth-child(2) div.entry-content' >> "$HTML_FILE"
-done
+curl https://www.theickabog.com$LC/read-the-story/ | pup 'ul.chapters__list a json{}' | jq -r '[.[] | {url: .href, chapter: .children[0].children[0].children[0].children[0].text, title: .children[0].children[0].children[0].children[1].text}] | sort_by(.chapter) | .[]|[.chapter, .title, .url] | @tsv' | while IFS=$'\t' read -r chapter title url; do download_chapter "$url" "$chapter" "$title"; done
 
 echo "</body></html>" >> "$HTML_FILE"
 
-pandoc --from=html --to=pdf \
-    --output=ickabog1.pdf \
-    --metadata title="The Ickabog" \
-    --metadata author="J.K Rowling" \
-    --pdf-engine=xelatex \
-    --dpi=300 \
-    -V book \
-    -V lang=en-US \
-    -V geometry=margin=1.5cm \
-    "$HTML_FILE"
+#pandoc --from=html --to=pdf \
+#    --output=ickabog1.pdf \
+#    --metadata title="The Ickabog" \
+#    --metadata author="J.K Rowling" \
+#    --pdf-engine=xelatex \
+#    --dpi=300 \
+#    -V book \
+#    -V lang=en-US \
+#    -V geometry=margin=1.5cm \
+#    "$HTML_FILE"
 
-pdftk cover.pdf ickabog1.pdf cat output ickabog.pdf
+#pdftk cover.pdf ickabog1.pdf cat output ickabog.pdf
+
+LANG=$(curl https://www.theickabog.com$LC/read-the-story/ | pup 'html attr{lang}')
+
+echo "<dc:title id=\"epub-title-1\">$MAIN_TITLE</dc:title>" > metadata.xml
+echo "<dc:date>$(date -I)</dc:date>" >> metadata.xml
+echo "<dc:language>$LANG</dc:language>" >> metadata.xml
+echo "<dc:creator id="epub-creator-1" opf:role="aut">J.K Rowling</dc:creator>" >> metadata.xml
 
 pandoc --from=html --to=epub \
     --output=ickabog.epub \
     --epub-metadata=metadata.xml \
     --epub-cover-image=cover.jpg \
-    --metadata title="The Ickabog" \
+    --metadata title="$MAIN_TITLE" \
     "$HTML_FILE"
 
-pandoc --from=html --to=pdf \
-    -V fontsize=18pt \
-    --output=ickabog2.pdf \
-    --metadata title="The Ickabog" \
-    --metadata author="J.K Rowling" \
-    --pdf-engine=context \
-    -V margin-left=0cm \
-    -V margin-right=0cm \
-    -V margin-top=0cm \
-    -V margin-bottom=0cm \
-    -V geometry=margin=0cm \
-    -V lang=en-US \
-    "$HTML_FILE"
+#pandoc --from=html --to=pdf \
+#    -V fontsize=18pt \
+#    --output=ickabog2.pdf \
+#    --metadata title="The Ickabog" \
+#    --metadata author="J.K Rowling" \
+#    --pdf-engine=context \
+#    -V margin-left=0cm \
+#    -V margin-right=0cm \
+#    -V margin-top=0cm \
+#    -V margin-bottom=0cm \
+#    -V geometry=margin=0cm \
+#    -V lang=en-US \
+#    "$HTML_FILE"
 
-pdftk cover.pdf ickabog2.pdf cat output ickabog-large.pdf
+#pdftk cover.pdf ickabog2.pdf cat output ickabog-large.pdf
