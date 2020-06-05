69785c9a566b2ede60fc024ed8de009d621f6643
Alain Perry <alain.perry@gmail.com>
2020-06-05 19:26:42 +0200

    convert curl calls to wget

--- generate.sh
+++ generate.sh
@@ -4,9 +4,9 @@ mkdir -p html
 
 LC="/fr" # empty for english
 PUP=/root/go/bin/pup
-LANG=$(curl https://www.theickabog.com$LC/read-the-story/ | $PUP 'html attr{lang}')
+LANG=$(wget --quiet https://www.theickabog.com$LC/read-the-story/ -O- | $PUP 'html attr{lang}')
 HTML_FILE=ickabog.html
-MAIN_TITLE=$(curl https://www.theickabog.com$LC/read-the-story/ | $PUP 'ul.chapters__list a json{}' | jq -r '[.[] | {url: .href, chapter: .children[0].children[0].children[0].children[0].text, title: .children[0].children[0].children[0].children[1].text}] | sort_by(.chapter) | .[]|[.chapter, .title, .url] | @tsv' | grep $' 2\t' | while IFS=$'\t' read -r chapter title url; do echo "$title"; done)
+MAIN_TITLE=$(wget --quiet https://www.theickabog.com$LC/read-the-story/ -O- | $PUP 'ul.chapters__list a json{}' | jq -r '[.[] | {url: .href, chapter: .children[0].children[0].children[0].children[0].text, title: .children[0].children[0].children[0].children[1].text}] | sort_by(.chapter) | .[]|[.chapter, .title, .url] | @tsv' | grep $' 2\t' | while IFS=$'\t' read -r chapter title url; do echo "$title"; done)
 OUTPUT_DIR=out
 
 mkdir -p $OUTPUT_DIR
@@ -15,12 +15,13 @@ echo "<html><head><title>$MAIN_TITLE</title></head><body>" > "$HTML_FILE"
 
 function download_chapter() {
 	[[ $2 =~ 1$ ]] && MAIN_TITLE=$3
-    [ -s "html/$2.html" ] || wget --quiet "https://www.theickabog.com$1" -O "html/$2.html"
+    URL=$( [[ $1 =~ ^http ]] && echo "$1" || echo "https://www.theickabog.com$1" )
+    [ -s "html/$2.html" ] || wget --quiet "$URL" -O "html/$2.html"
     echo "<h2>$3</h2>" >> "$HTML_FILE"
     cat "html/$2.html" | $PUP 'article div.row:nth-child(2) div.entry-content' >> "$HTML_FILE"
 }
 
-curl https://www.theickabog.com$LC/read-the-story/ | $PUP 'ul.chapters__list a json{}' | jq -r '[.[] | {url: .href, chapter: .children[0].children[0].children[0].children[0].text, title: .children[0].children[0].children[0].children[1].text}] | sort_by(.chapter) | .[]|[.chapter, .title, .url] | @tsv' | while IFS=$'\t' read -r chapter title url; do download_chapter "$url" "$chapter" "$title"; done
+wget --quiet https://www.theickabog.com$LC/read-the-story/ -O- | $PUP 'ul.chapters__list a json{}' | jq -r '[.[] | {url: .href, chapter: .children[0].children[0].children[0].children[0].text, title: .children[0].children[0].children[0].children[1].text}] | sort_by(.chapter) | .[]|[.chapter, .title, .url] | @tsv' | while IFS=$'\t' read -r chapter title url; do download_chapter "$url" "$chapter" "$title"; done
 
 echo "</body></html>" >> "$HTML_FILE"
 
@@ -39,13 +40,14 @@ echo "</body></html>" >> "$HTML_FILE"
 
 echo "<dc:title id=\"epub-title-1\">$MAIN_TITLE</dc:title>" > metadata.xml
 echo "<dc:date>$(date -I)</dc:date>" >> metadata.xml
-echo "<dc:language>$LANG</dc:language>" >> metadata.xml
+echo "<dc:lang>$LANG</dc:lang>" >> metadata.xml
 echo "<dc:creator id="epub-creator-1" opf:role="aut">J.K Rowling</dc:creator>" >> metadata.xml
 
 pandoc --from=html --to=epub \
     --output="$OUTPUT_DIR/ickabog.epub" \
     --epub-metadata=metadata.xml \
     --epub-cover-image=cover.jpg \
+	--epub-chapter-level=2 \
     --metadata title="$MAIN_TITLE" \
     "$HTML_FILE"
 
